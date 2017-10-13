0a92365a7b9901005ce6076ec4dc46b5de881d30
zhulx <17060801@cn.suning.com>
2017-10-13 18:04:56 +0800

    修改数据库导入

--- import_uqer.py
+++ import_uqer.py
@@ -1,30 +1,23 @@
 # -*- coding: utf-8 -*-
 
 import os
-
-import pandas as pd
-
-import pymysql
+import platform
 
 base_path = os.path.dirname(os.path.abspath(__file__))
 data_path = os.path.join(base_path, "data", "uqer")
 print(data_path)
 
-command_pre = '''mysql -uquant -p123456 -h127.0.0.1 quant --local-infile=1 -e 'load data local infile "'''
+is_windows = platform.system() == 'Windows'
 
-command_suffix = '''" into table uqer_stock_hs
-(
-  `code`,
-  `biz_date`,
-  `pe`,
-  `pb`,
-  `ps`,
-  `pcf`
-)' '''
+command_pre = '''mysql -uquant -p123456 -h127.0.0.1 quant --local-infile=1 -e "load data local infile \''''
+
+command_suffix = '''\' into table uqer_stock_hs(`code`, `biz_date`, `pe`, `pb`, `ps`, `pcf`)" '''
 
 
 for file_name in os.listdir(data_path):
     file = os.path.join(data_path, file_name)
-    os.system(command_pre + file + command_suffix)
-
+    if is_windows:
+        file = file.replace('\\', '\\\\')
+    command = command_pre + file + command_suffix
+    os.system(command)
 
