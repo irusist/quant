60dac4df7031a958f630d8910da1dc3371ffd82a
zhulx <zhulixin@chezhibao.com>
2018-09-14 19:38:41 +0800

    add 20180913 data

--- realtime_get_hk.py
+++ realtime_get_hk.py
@@ -191,6 +191,8 @@ code_list += ['08482.HK']
 code_list += ['08609.HK']
 # 2018-09-10
 code_list += ['01615.HK']
+# 2018-09-13
+code_list += ['08601.HK', '01969.HK']
 
 print(code_list)
 print(len(code_list))
