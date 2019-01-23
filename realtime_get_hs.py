39fbd4eba8f80a2a97e24a29016421b60087f5fb
zhulx <zhulixin@chezhibao.com>
2019-01-23 18:29:15 +0800

    add 20190122 data

--- realtime_get_hs.py
+++ realtime_get_hs.py
@@ -304,6 +304,8 @@ code_list += ['603739.SH', '002948.SZ']
 code_list += ['603332.SH', '002945.SZ']
 # 2019-01-21
 code_list += ['601298.SH']
+# 2019-01-22
+code_list += ['603700.SH']
 
 
 # code_list.sort()
