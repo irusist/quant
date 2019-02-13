681e11dac129012badc1f66423b186ee963a6c46
zhulx <zhulixin@chezhibao.com>
2019-02-13 08:37:04 +0800

    add 20190212 data

--- realtime_get_hk.py
+++ realtime_get_hk.py
@@ -21,6 +21,7 @@ headers = {
 
 
 today = date.today().strftime('%Y%m%d')
+# today = '20190212'
 base_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "xueqiu", "hk", today)
 if not os.path.exists(base_path):
     os.mkdir(base_path)
