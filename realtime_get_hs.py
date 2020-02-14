91654d1947aeae9456e9269615e89af6b062a70e
irusist <zhulx521@gmail.com>
2020-02-14 21:40:38 +0800

    add 20200213 data

--- realtime_get_hs.py
+++ realtime_get_hs.py
@@ -591,6 +591,8 @@ code_list += ['300816.SZ']
 code_list += ['688186.SH', '688398.SH']
 # 2020-02-12
 code_list += ['688080.SH', '300818.SZ']
+# 2020-02-13
+code_list += ['688208.SH', '300820.SZ']
 
 
 # code_list.sort()
